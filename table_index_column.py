46d4f44d751e3d6a61db0777877b9b0ceb2ac0bf
richardgmcmahon <rgm@ast.cam.ac.uk>
2015-08-12 08:42:10 +0100

    changes to docstring 	modified:   table_index_column.py

--- table_index_column.py
+++ table_index_column.py
@@ -1,15 +1,19 @@
 def table_index_column(table=None, colname=None, casesensitive=False, debug=False):
   """
 
+  see also astropy.Table.index_column
+
+  http://astropy.readthedocs.org/en/latest/api/astropy.table.Table.html#astropy.table.Table.index_column
+  
+  could deprecate and use the astropy version and make the 
+  case sensitive support via either changing the colnames in main code
+  or via trying both cases sequentially.
+
   astropy table function; could be modified to also support pyfits and
   Erin Sheldons fitsio
  
   includes non case sensitive option
 
-  see also astropy.Table.index_column
-
-http://astropy.readthedocs.org/en/latest/api/astropy.table.Table.html#astropy.table.Table.index_column
-  
   return icol = -1 if no column exists
 
 
