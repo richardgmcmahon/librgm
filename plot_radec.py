95dcdcdf0542ba9b5dc2276313337df951e3e392
richardgmcmahon <rgm@ast.cam.ac.uk>
2015-08-24 21:56:28 +0100

    added support for aspect ratio, units and plotid 	modified:   plot_radec.py

--- plot_radec.py
+++ plot_radec.py
@@ -1,9 +1,10 @@
 
 from matplotlib import pyplot as plt
+from librgm.plotid import plotid
 
 def plot_radec(ra, dec, title=None, xlabel=None, ylabel=None,
   rarange=None, decrange=None, showplots=False, figfile=None,
-  figsize=(10.0,10.0),
+  figsize=(10.0,10.0), units=None, aspect=None, noplotid=False,
   plotdir='./'):
 
   #plt.setp(lines, edgecolors='None')
@@ -24,14 +25,20 @@ def plot_radec(ra, dec, title=None, xlabel=None, ylabel=None,
   ydata=dec
 
 
-  print min(xdata), max(xdata)
-  print min(ydata), max(ydata)
+  print('RA range: ', min(xdata), max(xdata))
+  print('Dec range: ', min(ydata), max(ydata))
+  print('units: ', units)
 
   if rarange is None:
     plt.xlim([0,24.0])
 
   if decrange is None:    
     plt.ylim([-90,30])
+
+  if units == 'Degrees':
+    print('units: ', units)
+    plt.xlim([min(xdata),max(xdata)])
+    plt.ylim([min(ydata),max(ydata)])
 
   ms=1.0
   plt.plot(xdata, ydata, 'ob', markeredgecolor='b', ms=ms)
@@ -41,6 +48,10 @@ def plot_radec(ra, dec, title=None, xlabel=None, ylabel=None,
   print 'Number of data points plotted: ', ndata
   plt.legend([
    'n: '+ str(ndata)])
+  plotid(progname=True)
+
+  if aspect == 'equal':
+    plt.axes().set_aspect('equal')
 
   if showplots: plt.show()
 
