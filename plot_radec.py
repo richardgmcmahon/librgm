6cc9b7eaccd98c39d2d15e3d18c2a75fd97722fd
richardgmcmahon <rgm@ast.cam.ac.uk>
2015-09-12 15:35:36 +0100

    added overplot option 	modified:   plot_radec.py

--- plot_radec.py
+++ plot_radec.py
@@ -3,15 +3,16 @@ from matplotlib import pyplot as plt
 from librgm.plotid import plotid
 
 def plot_radec(ra, dec, title=None, xlabel=None, ylabel=None,
+  plotstyle=None, linestyle=None, markersize=None,
   rarange=None, decrange=None, showplots=False, figfile=None,
   figsize=(10.0,10.0), units=None, aspect=None, noplotid=False,
-  plotdir='./'):
+  plotdir='./', savefig=True, overplot=False):
 
   #plt.setp(lines, edgecolors='None')
 
   if figfile == None: figfile='radec.png'
 
-  plt.figure(num=None, figsize=figsize)
+  if not overplot: plt.figure(num=None, figsize=figsize)
 
   plt.xlabel('RA')
   if xlabel != None: plt.xlabel(xlabel)
@@ -31,17 +32,28 @@ def plot_radec(ra, dec, title=None, xlabel=None, ylabel=None,
 
   if rarange is None:
     plt.xlim([0,24.0])
+  if rarange is not None:
+    plt.xlim(rarange)
+
 
   if decrange is None:    
     plt.ylim([-90,30])
+  if decrange is not None:    
+    plt.ylim(decrange)
 
   if units == 'Degrees':
     print('units: ', units)
     plt.xlim([min(xdata),max(xdata)])
     plt.ylim([min(ydata),max(ydata)])
 
-  ms=1.0
-  plt.plot(xdata, ydata, 'ob', markeredgecolor='b', ms=ms)
+  if markersize is None: markersize=1.0
+  if plotstyle is None:
+    plt.plot(xdata, ydata, 'ob', markeredgecolor='b', markersize=markersize)
+
+  if plotstyle is not None:
+    print('plotstyle: ', plotstyle)
+    plt.plot(xdata, ydata, plotstyle, 
+     markeredgecolor=None, markersize=markersize)
   #plotid.plotid()
 
   ndata=len(xdata)
@@ -55,7 +67,8 @@ def plot_radec(ra, dec, title=None, xlabel=None, ylabel=None,
 
   if showplots: plt.show()
 
-  print 'Saving: ', figfile
-  plt.savefig(figfile)
+  if savefig: 
+   print 'Saving: ', figfile
+   plt.savefig(figfile)
 
 
