0959b315dd4375d7461b63d19552c76d9562d778
richardgmcmahon <rgm@ast.cam.ac.uk>
2016-03-29 22:56:02 +0100

    made pep8 compliant 	modified:   table_stats.py

--- table_stats.py
+++ table_stats.py
@@ -1,118 +1,127 @@
 # $Source: /Users/rgm/soft/python/rgm/RCS/table_test.py,v $
-# $Id: table_test.py,v 1.1 2009/08/18 11:09:42 rgm Exp rgm $     
+# $Id: table_test.py,v 1.1 2009/08/18 11:09:42 rgm Exp rgm $
+from __future__ import print_function, division
 
 
 def table_stats(data, ext=1, verbose=True, debug=False):
-  """
-  pyfits function to get information about a table
+    """
+    pyfits function to get information about a table
 
-  USE:
-  > python
-  >>> from table_stats import *
-  >>> help(table_stats)
+    USE:
+    > python
+    >>> from table_stats import *
+    >>> help(table_stats)
 
-  TODO:
-  pyfits.info() for the object that has been read
-  see pyfits(infile).info
+    TODO:
+    pyfits.info() for the object that has been read
+    see pyfits(infile).info
 
-  Original: Richard McMahon
+    Original: Richard McMahon
 
-  $Id: table_test.py,v 1.1 2009/08/18 11:09:42 rgm Exp rgm $     
+    $Id: table_test.py,v 1.1 2009/08/18 11:09:42 rgm Exp rgm $
 
-  $Log: table_test.py,v $
-  Revision 1.1  2009/08/18 11:09:42  rgm
-  Initial revision
+    $Log: table_test.py,v $
+    Revision 1.1  2009/08/18 11:09:42  rgm
+    Initial revision
 
-  Login name of author of last revision:   $Author: rgm $ 
-  Date and time (UTC) of revision:         $Date: 2009/08/18 11:09:42 $
-  Login name of user locking the revision: $Locker: rgm $ 
-  CVS revision number:                     $Revision: 1.1 $ 
-  """
+    Login name of author of last revision:   $Author: rgm $
+    Date and time (UTC) of revision:         $Date: 2009/08/18 11:09:42 $
+    Login name of user locking the revision: $Locker: rgm $
+    CVS revision number:                     $Revision: 1.1 $
+    """
 
-  __version__ = "$Revision: 1.2 $"
-  __date__ = "$Date: 2009/08/18 11:09:42 $"
+    __version__ = "$Revision: 1.2 $"
+    __date__ = "$Date: 2009/08/18 11:09:42 $"
 
-  import sys
-  import time
+    import sys
+    import time
 
-  import traceback
+    import traceback
 
-  import pyfits
+    import pyfits
 
-  import numpy as np
-  
-  from pause import *
+    import numpy as np
 
-  print 'Executing: ',sys.argv[0], __version__
+    from pause import *
 
-  if debug: help(data)
+    print('Executing:', sys.argv[0], __version__)
 
-  #data, hdr = pyfits.getdata(infile, ext, header=True)
+    if debug:
+        help(data)
 
-  infile=data
+    # data, hdr = pyfits.getdata(infile, ext, header=True)
 
-  if isinstance(data,np.ndarray): 
-    print 'isInstance: numpy.ndarray'
-    ncolumns=len(data.columns)
-    nrows=len(data)
- 
-  print 'Infile: ', data
-  print 'Extension: ', ext
+    infile = data
 
-  if not isinstance(data,np.ndarray): 
-    print 'NOT isInstance: numpy.ndarray'
-    hdr = pyfits.getheader(infile,ext)
-    naxis2=hdr['naxis2']
-    naxis1=hdr['naxis1']
-    print 'naxis1: ',naxis1
-    print 'naxis2: ',naxis2
+    if isinstance(data, np.ndarray):
+        print('isInstance: numpy.ndarray')
+        ncolumns = len(data.columns)
+        nrows = len(data)
+
+    print('Infile:', data)
+    print('Extension:', ext)
+
+    if not isinstance(data, np.ndarray):
+        print('NOT isInstance: numpy.ndarray')
+        hdr = pyfits.getheader(infile, ext)
+        naxis2 = hdr['naxis2']
+        naxis1 = hdr['naxis1']
+        print('naxis1:', naxis1)
+        print('naxis2:', naxis2)
 
     if naxis2 > 0:
-      data = pyfits.open(infile)[ext].data
-      if debug: help(data)
+        data = pyfits.open(infile)[ext].data
+        if debug:
+            help(data)
 
-      header = pyfits.open(infile)[ext].header
-      if debug: help(header)
+        header = pyfits.open(infile)[ext].header
+        if debug:
+            help(header)
 
-      columns = pyfits.open(infile)[ext].columns
-      if debug: help(columns)
+        columns = pyfits.open(infile)[ext].columns
+        if debug:
+            help(columns)
 
-      ncolumns = len(columns)
+        ncolumns = len(columns)
 
-      print 'Number of columns: ', len(columns)
-      print 'Number of rows: ',len(data)
+        print('Number of columns:', len(columns))
+        print('Number of rows:', len(data))
 
+    # help(data.columns.names)
+    nrows = len(data)
+    # : are used to identify empty strings from string variables
 
-  #help(data.columns.names)
-  nrows=len(data)
-  # : are used to identify empty strings from string variables
+    for i in xrange(ncolumns):
+        j = 0
+        # process the columns that are 1D vectors
+        if len(data.field(i).shape) == 1:
+            try:
+                print(i, j, data.columns[i].name, data.columns[i].format,
+                      data.columns[i].dim, data.field(i).shape,
+                      len(data.field(i).shape),
+                      len(data.field(i)), ': ',
+                      np.min(data.field(i)), ': ',
+                      np.max(data.field(i)))
 
-  for i in xrange(ncolumns):
-      j=0
-      # process the columns that are 1D vectors
-      if len(data.field(i).shape) == 1:
-        try:
-          print i, j, data.columns[i].name, data.columns[i].format, \
-           data.columns[i].dim, data.field(i).shape, len(data.field(i).shape), \
-           len(data.field(i)), \
-           ': ',np.min(data.field(i)), ' : ',np.max(data.field(i)) 
-        except:
-          # try min, max rather than np.min, np.max
-          print i, j, data.columns[i].name, 'problem with column'
-          print i, j, data.columns[i].name, data.columns[i].format, \
-           data.columns[i].dim, data.field(i).shape, len(data.field(i).shape), \
-           len(data.field(i)), \
-           ': ', min(data.field(i)), ' : ', max(data.field(i)) 
+            except:
+                # try min, max rather than np.min, np.max
+                print(i, j, data.columns[i].name, 'problem with column')
+                print(i, j, data.columns[i].name, data.columns[i].format,
+                      data.columns[i].dim, data.field(i).shape,
+                      len(data.field(i).shape),
+                      len(data.field(i)), ': ',
+                      min(data.field(i)), ': ', max(data.field(i)))
 
-          pass
+                pass
 
-      # process the columns that are 2D vectors (i,j)
-      if len(data.field(i).shape) == 2:
-        for j in xrange(data.field(i).shape[1]):
-          print i,j, columns[i].name, columns[i].format, \
-           columns[i].dim, data.field(i).shape, \
-           len(data.field(i).shape), \
-           len(data.field(i)), \
-           ': ',np.min(data.field(i)[j]),' : ',np.max(data.field(i)[j])
+        # process the columns that are 2D vectors (i,j)
+        if len(data.field(i).shape) == 2:
+            for j in xrange(data.field(i).shape[1]):
+                print(i, j, columns[i].name, columns[i].format,
+                      columns[i].dim, data.field(i).shape,
+                      len(data.field(i).shape),
+                      len(data.field(i)), ': ',
+                      np.min(data.field(i)[j]), ': ',
+                      np.max(data.field(i)[j]))
 
-  return
+    return
