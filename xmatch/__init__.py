fcc8aa3adb9ff4f58c98be6a841dc73aac167b0f
richardgmcmahon <rgm@ast.cam.ac.uk>
2017-01-17 23:23:24 +0000

    new file:   __init__.py

---
+++ xmatch/__init__.py
@@ -0,0 +1,7 @@
+from .xmatch_cat import *
+from .xmatch_cat_join import *
+from .xmatch_checkplot import *
+from .xmatch_checkplot0 import *
+from .xmatch_checkplots0 import *
+from .xmatch_selfcheck import *
+from .add_columns_spherical_offsets import *
