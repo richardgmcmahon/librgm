2a69b6edac3da510881903321994508772228ad8
richardgmcmahon <rgm@ast.cam.ac.uk>
2017-01-23 20:28:05 +0000

    new file:   xmatch_cat_join.py

---
+++ xmatch/xmatch_cat_join.py
@@ -0,0 +1,25 @@
+from __future__ import print_function, unicode_literals
+
+def xmatch_cat_join(table1=None, table2=None,
+                    idxmatch=None,
+                    dr=None, radius_arcsec=2.0):
+    """
+
+
+    """
+    from astropy.table import Table, hstack
+
+    # rows in table2 that match with table1
+    table2_xmatch_table1 = table2[idxmatch]
+
+    # paste table2 columns onto the table1 rows
+    print(len(table2_xmatch_table1))
+    result = hstack([table1, table2_xmatch_table1])
+    print('Number of rows in hstack table:', len(result))
+
+    # create table with sources that match with in radius of 10"
+    itest = dr < radius_arcsec
+    print('Number of matched sources within 10" arcsec:', len(table1[itest]))
+    result = result[itest]
+
+    return result
