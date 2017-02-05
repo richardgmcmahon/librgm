3dee2239d83a6d09d87c97be77c6ef4ebcc66530
richardgmcmahon <rgm@ast.cam.ac.uk>
2017-02-05 23:48:14 +0000

    better documentation of inpect compared with traceback 	modified:   lineno.py

--- util/lineno.py
+++ util/lineno.py
@@ -1,31 +1,64 @@
 from __future__ import print_function
-"""This provides a lineno() function to make it easy to grab the line
-number that we're on.
 
-
-using inspect: see
-http://code.activestate.com/recipes/145297-grabbing-the-current-line-number-easily/
-
-
-Danny Yoo (dyoo@hkn.eecs.berkeley.edu)
-
-http://python.org/doc/lib/module-inspect.html
-
-
-It can also be done with traceback; see plotid.py
-
-see also http://stackoverflow.com/questions/5067604/determine-function-name-from-within-that-function-without-using-traceback
-Note inspect.stack calls are thousands of times slower than the alternatives:
-
-
-"""
 import sys
 import inspect
 import traceback
 
 def lineno(USEtraceback=False, functionName=True, fileName=True,
-           debug=False):
-    """Returns the current line number in our program."""
+           debug=False, verbose=False):
+
+    """Return current function line number, name and filename
+
+
+    loosely based on code by Danny Yoo (dyoo@hkn.eecs.berkeley.edu)
+
+    using inspect: see
+    http://code.activestate.com/recipes/145297-grabbing-the-current-line-number-easily/
+
+    http://python.org/doc/lib/module-inspect.html
+
+    It can also be done with traceback; see plotid.py
+
+    https://docs.python.org/2/library/traceback.html
+
+
+    see also http://stackoverflow.com/questions/5067604/determine-function-name-from-within-that-function-without-using-traceback
+
+    Note inspect.stack calls are thousands of times slower than the
+    alternatives:
+
+    Also inspect.stack and traceback.extract_stack return stack entries
+    in a different order.
+
+    traceback returns a 4-tuple: (filename, line number, function name*, text)
+    e.g. ('lineno.py', 58, 'lineno', 'stack = traceback.extract_stack()')
+
+    inspect returns a 6-tuple:
+
+     (frame object, filename, line number, function name,
+      lines of context from the source code, and
+      the index of the current line within that list)
+
+    e.g.
+
+    ( <frame object at 0x100543370>, 'lineno.py', 51, 'lineno',
+     ['        stack = inspect.stack()\n'], 0)
+
+
+    Note:
+
+    You have to return the current level + 1 with inspect to get the
+    line number information for the calling fucntion i.e. [1] for the index;
+    [0] is this function. [-1] would be the main.
+
+    Traceback start at the other end so needs [-2] for the index to
+    the calling function [-] refers to this function. [0] would be
+    the main function.
+
+
+
+    """
+
 
     if debug:
 
@@ -43,49 +76,71 @@ def lineno(USEtraceback=False, functionName=True, fileName=True,
             print(ilevel, stack[ilevel])
         print()
 
-        print('inspect.stack()[1][0]):function:',
-              inspect.stack()[1][0])
-        print('inspect.stack()[1][1]):function:',
-              inspect.stack()[1][1])
-        print('inspect.stack()[1][2]):function:',
-              inspect.stack()[1][2])
-        print('inspect.stack()[1][3]):function:',
-              inspect.stack()[1][3])
+        print('traceback.extract_stack()[-1][2]:',
+              traceback.extract_stack()[-1][2])
 
-        print('inspect.stack()[0][0].f_code.co_name:function',
+        print('inspect.stack()[0][0]:',
+              inspect.stack()[0][0])
+
+        print('inspect.stack()[0][3]:',
+              inspect.stack()[0][3])
+
+        print('inspect.stack()[0][0].f_code.co_name:',
               inspect.stack()[0][0].f_code.co_name)
 
-        print('inspect.currentframe().f_code.co_name:function:',
+        print('inspect.currentframe().f_code.co_name:',
               inspect.currentframe().f_code.co_name)
 
-        print('sys._getframe().f_code.co_name:function:',
+        print('sys._getframe().f_code.co_name:',
               sys._getframe().f_code.co_name)
 
     if USEtraceback:
-        lineno = traceback.extract_stack()[0][1]
+        lineno = traceback.extract_stack()[-1][1]
+
+        result = lineno
+
+        if functionName:
+            functionname = traceback.extract_stack()[-1][3]
+            result = (functionname, lineno)
+
+        if fileName:
+            filename = traceback.extract_stack()[-1][3]
+            result = (lineno, filename)
+
+        if fileName and functionName:
+            result = (functionname, lineno, filename)
+
+
 
     if not USEtraceback:
-        lineno = inspect.currentframe().f_back.f_lineno
+        # lineno = inspect.currentframe().f_back.f_lineno
+        lineno = inspect.stack()[1][2]
 
-    lineno = inspect.stack()[1][2]
-    result = lineno
+        result = lineno
 
-    if functionName:
-        functionname = inspect.stack()[1][3]
-        result = (functionname, lineno)
+        if functionName:
+            functionname = inspect.stack()[1][3]
+            result = (functionname, lineno)
 
+        if fileName:
+            filename = inspect.stack()[1][1]
+            result = (lineno, filename)
 
-    if fileName:
-        filename = inspect.stack()[1][1]
-        result = (lineno, filename)
+        if fileName and functionName:
+            result = (functionname, lineno, filename)
 
-    if fileName and functionName:
-        result = (functionname, lineno, filename)
+    if verbose:
+        print('INFO:', __file__, __name__, [str(i) for i in result])
 
     return result
 
 if __name__ == '__main__':
 
-    print("hello, this is line number using inspect", lineno())
     print()
-    print("and now use tracback; this is line", lineno(USEtraceback=True))
+    print("hello, this is line number using inspect:\n",
+          lineno(debug=True, verbose=True))
+    print()
+    print()
+    print("and now use tracback; this is line:\n",
+          lineno(USEtraceback=True, debug=True, verbose=True))
+    print()
