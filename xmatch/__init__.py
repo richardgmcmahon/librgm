792e6e38c0c9b3da834eaa4bfd3f38d505e32323
richardgmcmahon <rgm@ast.cam.ac.uk>
2017-02-27 23:36:36 +0000

    add xmatch_groups 	modified:   __init__.py

--- xmatch/__init__.py
+++ xmatch/__init__.py
@@ -1,4 +1,14 @@
+"""
+this will allow import and call of the form
+
+from librgm.xmatch import xmatch_groups
+idxmatch1, idxmatch2, dr = \
+    xmatch_groups(table1=table,
+                  colnames_radec1=colnames_radec,
+
+"""
 from .xmatch_cat import *
+from .xmatch_groups import *
 from .xmatch_cat_join import *
 from .xmatch_checkplot import *
 from .xmatch_checkplot0 import *
