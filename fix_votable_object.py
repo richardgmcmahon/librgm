3959d49e41f97440fc1318fff0415f0dc07d35f1
richardgmcmahon <rgm@ast.cam.ac.uk>
2018-04-07 03:28:20 +0100

    added verbose option to turn of chatter 	modified:   fix_votable_object.py

--- fix_votable_object.py
+++ fix_votable_object.py
@@ -1,17 +1,18 @@
-def fix_votable_object(table):
+def fix_votable_object(table, verbose=False):
+    """
+    convert the columns with dtype = object which is not
+    supported by FITs to bool
     """
 
-    """
     for (icol, column) in enumerate(table.columns):
-        print(icol, table.columns[icol].name,
-              table.columns[icol].format,
-              table.columns[icol].dtype)
-        # convert the columns for dtype = object which is not
-        # supported by FITs to bool
+        if verbose:
+            print(icol, table.columns[icol].name,
+                table.columns[icol].format,
+                table.columns[icol].dtype)
+
         if table.columns[icol].dtype == 'object':
             colname = table.columns[icol].name
             NewColumn = Table.Column(table[colname].data, dtype='bool')
             table.replace_column(colname, NewColumn)
-    print()
 
     return table
