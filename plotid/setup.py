06a56ffe05ccd41f00c674f786a632281e6a19b7
richardgmcmahon <rgm@ast.cam.ac.uk>
2020-05-10 12:30:08 +0100

    new file:   plotid/README.setup 	new file:   plotid/plotid.py 	new file:   plotid/setup.py

---
+++ plotid/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import distutils
+from distutils.core import setup
+
+setup(name='plotid',
+      version='1.0',
+      description='Add provenance metadata to a plot',
+      author='richardgmcmahon',
+      author_email='rgm@ast.cam.ac.uk',
+      license='GNU GPL v3+',
+      py_modules=['plotid']
+     )
